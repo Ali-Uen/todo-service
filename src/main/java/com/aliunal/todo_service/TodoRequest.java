77b132289082c0589af00bee82cdab4e9fbc16e4
Ali Ünal <65597936+Ali-Uen@users.noreply.github.com>
2025-09-23 12:07:32 +0200

    refactor: remove duplicate package structure

--- src/main/java/com/aliunal/todo_service/TodoRequest.java
+++
@@ -1,7 +0,0 @@
-package com.aliunal.todo_service;
-import jakarta.validation.constraints.NotBlank;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record TodoRequest(
-  @NotBlank String title, String description,
-  @JsonProperty("completed") Boolean done) {}
